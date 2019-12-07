312ef68cec9f2f92086639eb1e1e9b9d17f82d78
Nick <niiickolay@gmail.com>
2019-12-07 17:24:31 +0200

    Create client

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Main from './scenes/Main';
+
+import './styles/reset.scss';
+import './styles/typo.scss';
+import './styles/common.scss';
+
+ReactDOM.render(<Main />, document.getElementById('root'));
