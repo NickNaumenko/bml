cfcfbed64c7affe0e136565b220b64e34020ee39
Nick <niiickolay@gmail.com>
2019-12-09 21:47:18 +0200

    Add breadcrumbs to UserPage

--- client/src/scenes/UserPage/index.jsx
+++ client/src/scenes/UserPage/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Breadcrumbs from '../../components/Breadcrumbs';
+import routes from '../../routes';
 
-const UserPage = () => (
-  <h1>User page</h1>
-);
+const UserPage = () => {
+  const breadcrumbs = [routes.home, routes.users, {name: 'user name'}];
+  return (
+    <>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+    </>
+  )
+};
 
 export default UserPage;
