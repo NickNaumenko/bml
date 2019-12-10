9bb78a6b302aa60a66d6c2729e30da910f9cbd78
Nick <niiickolay@gmail.com>
2019-12-10 22:22:33 +0200

    Implement selecting stats with date condition

--- server/data/repositories/usersStatsRepository.js
+++ server/data/repositories/usersStatsRepository.js
@@ -6,10 +6,15 @@ const pathToStatistic = path.resolve(__dirname, '../users_statistic.json');
 class UserStatsRepository extends BaseRepository {
   async getByUserId(userId, query) {
     const { from, to } = query;
-    console.log(from, to);
-    const stats = await this.select()
-      .and({attr: 'user_id', op: '=', value: userId})
-      .run();
+    const select = await this.select()
+      .and({attr: 'user_id', op: '=', value: userId});
+    if (from) {
+      select.and({attr: 'date', op: '>=', value: from});
+    }
+    if (to) {
+      select.and({attr: 'date', op: '<=', value: to});
+    }
+    const stats = select.run();
 
     return stats;
   }
