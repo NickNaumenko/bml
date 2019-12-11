a094ac2b16bf0f4ecc8af461fa0761709c80803b
Nick <niiickolay@gmail.com>
2019-12-11 21:26:53 +0200

    Fix formatDate function

--- server/helpers/formatDate.js
+++ server/helpers/formatDate.js
@@ -1,7 +1,12 @@
 const formatDate = date => {
   const dateNumber = Number(date);
-  const startDate = new Date(dateNumber);
-  return `${startDate.getFullYear()}-${startDate.getMonth()}-${startDate.getDate()}`;
+  const stringDate = new Date(dateNumber);
+
+  const format = date => date < 10 ? `0${date}` : date;
+  const formattedMonth = format(stringDate.getMonth() + 1);
+  const formattedDate = format(stringDate.getDate());
+
+  return `${stringDate.getFullYear()}-${formattedMonth}-${formattedDate}`;
 }
 
 module.exports = formatDate;
