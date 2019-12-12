f4526c9de4bb3405730e89c10e2203a9aceaed69
Nick <niiickolay@gmail.com>
2019-12-12 16:16:27 +0200

    Add error handler middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./api/routes');
 const cors = require('cors');
+const errorHandlerMiddleware = require('./api/middlewares/errorHandlerMiddleware');
 
 const app = express();
 app.use(cors());
 
 routes(app);
 
+app.use(errorHandlerMiddleware);
 app.listen(3001, () => {
   console.log('listening on port 3001');
 });
